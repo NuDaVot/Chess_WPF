08fc5e3c6670b819ecc263fcef5cc9ac2d2661d5
MiratSSS <eror9911@mail.ru>
2023-03-22 09:56:33 +0500

    Design p1

---
+++ Chess2/Chess2/Menu(Authorized).xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Chess2
+{
+    /// <summary>
+    /// Логика взаимодействия для Menu_Authorized_.xaml
+    /// </summary>
+    public partial class Menu_Authorized_ : Window
+    {
+        public Menu_Authorized_()
+        {
+            InitializeComponent();
+        }
+    }
+}
