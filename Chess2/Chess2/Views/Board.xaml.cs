e468b33c8e42a7425eb23235821c7274da8b282f
NuDaVot <nudanavernoevot@gmail.com>
2023-04-01 08:18:25 +0500

    Board

---
+++ Chess2/Chess2/Views/Board.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chess2.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для Board.xaml
+    /// </summary>
+    public partial class Board : Page
+    {
+        public Board()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 8; i++)
+            {
+                for(int j = 0; j < 8; j++)
+                {
+                    Border myBorder = new Border();
+                    myBorder.SetBinding(Grid.RowProperty, $"location_cells_row[{i}]");
+                    myBorder.SetBinding(Grid.ColumnProperty, $"location_cells_column[{j}]");
+                    if ((i + j) % 2 == 0)
+                    {
+                        myBorder.Style = (Style)myBorder.FindResource("transparent_border");
+                    }
+                    else
+                    {
+                        myBorder.Style = (Style)myBorder.FindResource("red_border");
+                    }
+                    
+                    fild_borders.Children.Add(myBorder);
+                    Grid.SetZIndex(myBorder, 2);
+
+                    Grid grid = new Grid();
+                    grid.RowDefinitions.Add(new RowDefinition());
+                    grid.RowDefinitions.Add(new RowDefinition());
+                    grid.RowDefinitions.Add(new RowDefinition());
+                    grid.ColumnDefinitions.Add(new ColumnDefinition());
+                    grid.ColumnDefinitions.Add(new ColumnDefinition());
+                    grid.ColumnDefinitions.Add(new ColumnDefinition());
+                    grid.SetBinding(Grid.RowProperty, $"location_cells_row[{i}]");
+                    grid.SetBinding(Grid.ColumnProperty, $"location_cells_column[{j}]");
+
+                    Ellipse ell = new Ellipse();
+                    Grid.SetRow(ell, 1);
+                    Grid.SetColumn(ell, 1);
+                    ell.Style = (Style)ell.FindResource("turquoise_ellipse");
+                    ell.SetBinding(VisibilityProperty, $"visibility_ellipses[{i}][{j}]");
+
+                    grid.Children.Add(ell);
+                    Grid.SetZIndex(grid, 4);
+
+                    fild_borders.Children.Add(grid);
+                }
+            }
+            
+        }
+    }
+}
--- Chess2/Chess2/obj/Debug/net7.0-windows/Views/Profile.g.i.cs
+++ Chess2/Chess2/obj/Debug/net7.0-windows/Views/Profile.g.i.cs
@@ -52,13 +52,13 @@ namespace Chess2.Views {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Chess2;V1.0.0.0;component/views/profile.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Chess2;component/views/profile.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\Profile.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -68,7 +68,7 @@ namespace Chess2.Views {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
