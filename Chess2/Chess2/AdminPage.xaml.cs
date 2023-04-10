b5dc2f749a7ff1ee1b624e034ea779ec98082f88
MiratSSS <eror9911@mail.ru>
2023-04-10 15:42:20 +0500

    Design AdminPage ListView startVar

--- Chess2/Chess2/AdminPage.xaml.cs
+++ Chess2/Chess2/AdminPage.xaml.cs
@@ -10,20 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Chess2
 {
     /// <summary>
-    /// Логика взаимодействия для Leaderboard.xaml
+    /// Логика взаимодействия для AdminPage.xaml
     /// </summary>
-    public partial class Leaderboard : Window
+    public partial class AdminPage : Window
     {
-        public Leaderboard()
+        public AdminPage()
         {
             InitializeComponent();
         }
-
     }
 }
