eb394b964dfab57f289bb91519df6052747dc586
Walkerbe-ai <akim.mustafaev.2016@mail.ru>
2023-09-29 13:20:57 +0500

    .

--- Chess2/Chess2/Model/LeaderboardModel.cs
+++ Chess2/Chess2/Model/LeaderboardModel.cs
@@ -1,24 +1,35 @@
-﻿
-namespace Chess2.Model
+﻿namespace Chess2.Model
 {
-    class LeaderboardModel : BindableBase
+    public class LeaderboardModel : BindableBase
 	{
 		MainWindowModel _mainWindow;
-		public static bool MenuAOrMenu;
-		public string Jackdaw = "/Resources/Pictures/Jackdaw.png";
-		public LeaderboardModel()
+        HistoryChessContext _historyChessContext;
+
+
+        public static bool MenuAOrMenu;
+		public string Ramka = "/Resources/Pictures/Ramka.png";
+        public string King = "/Resources/Pictures/King.png";
+        public string Nick { get; set; }
+        public int Place { get; set; }
+        public int? Rating { get; set; }
+        public List<User> GetAllUser() => _historyChessContext.Users.ToList();
+
+        public LeaderboardModel(int place, string nick, int? rating)
 		{
 			_mainWindow = MainWindowViewModel._metod;
-			
-		}
+            _historyChessContext = new HistoryChessContext();
+            
+            Place = place;
+            Nick = nick;
+            Rating = rating;
+        }
 		public void IsCancel()
 		{
 			if (MenuAOrMenu)
-			{
-				_mainWindow.Navipage("MenuA.xaml");
-			}
-			else _mainWindow.Navipage("Menu.xaml");
+                _mainWindow.Navipage("MenuA.xaml");
+            else _mainWindow.Navipage("Menu.xaml");
 
 		}
-	}
+        
+    }
 }
--- Chess2/Chess2/ViewModels/LeaderboardViewModel.cs
+++ Chess2/Chess2/ViewModels/LeaderboardViewModel.cs
@@ -1,26 +1,71 @@
-﻿
-
-namespace Chess2.ViewModels
+﻿namespace Chess2.ViewModels
 {
     public class LeaderboardViewModel : BindableBase
     {
-		readonly LeaderboardModel _model = new LeaderboardModel();
-		public string Jackdaw => _model.Jackdaw;
-
-		public LeaderboardViewModel()
+		readonly LeaderboardModel _model = new LeaderboardModel(0, "",0);
+		public string Ramka => _model.Ramka;
+        public string King => _model.King;
+        public string Nick { get; set; }
+        public Visibility BV {  get; set; }
+        public string Search
         {
-			_model.PropertyChanged += (s, e) => RaisePropertiesChanged(e.PropertyName);
-			CancelCommand = new DelegateCommand<string>(str =>
-			{
-				_model.IsCancel();
-			});
-			//White = new DelegateCommand<string>(str =>
-			//{
-			//	_model.IsWhite();
-			//});
-		}
-		public DelegateCommand<string> CancelCommand { get; }
-		public DelegateCommand<string> SignUpCommand { get; }
+            get { return GetValue<string>(); }
+            set { SetValue(value, changedCallback: SearchPlayer); }
+        }
+        public int? Rating { get; set; }
+        public int place { get; set; } = 1;
+
+        public static ObservableCollection<LeaderboardModel> Players { get; set; } = new ObservableCollection<LeaderboardModel>();
+
+
+        public LeaderboardViewModel()
+        {
+            _model.PropertyChanged += (s, e) => RaisePropertiesChanged(e.PropertyName);
+            SearchPlayer();
+        }
+        public void SearchPlayer()
+        {
+            var personWithMaxAge = _model.GetAllUser().OrderByDescending(player => player.Rating).First();
+            Rating = personWithMaxAge.Rating;
+            Nick = personWithMaxAge.Nick;
+            var players = _model.GetAllUser().OrderByDescending(player => player.Rating);
+            var nick = _model.GetAllUser();
+            if (!string.IsNullOrWhiteSpace(Search))
+            {
+                Players.Clear();
+                nick = nick.Where(p => p.Nick.ToString().ToLower().Contains(Search.ToLower())).ToList();
+
+                foreach (var i in nick)
+                {
+                    if (i != personWithMaxAge)
+                    {
+                        BV = Visibility.Collapsed;
+                        Players.Add(new LeaderboardModel(place, i.Nick, i.Rating));
+
+                    }
+                    else
+                    {
+                        BV = Visibility.Visible;
+                        Players.Clear();
+                    }
+                }
+            }
+            else
+            {
+                Players.Clear();
+                BV = Visibility.Visible;
+                place = 0;
+                foreach (var i in players)
+                {
+                    place++;
+                    if (i != personWithMaxAge)
+                        Players.Add(new LeaderboardModel(place, i.Nick, i.Rating));
+                }
+            }
+           
+        }
+        public DelegateCommand CancelCommand => new(() => _model.IsCancel());
+        public DelegateCommand SignUpCommand { get; set; }
 	}
 
 }
