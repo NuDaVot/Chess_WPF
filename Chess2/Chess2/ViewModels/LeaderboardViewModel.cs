db861f69c916fcff90444fc585ac6b410a033ecc
Walkerbe-ai <akim.mustafaev.2016@mail.ru>
2023-09-29 14:25:09 +0500

    Перезалив

--- Chess2/Chess2/ViewModels/LeaderboardViewModel.cs
+++ Chess2/Chess2/ViewModels/LeaderboardViewModel.cs
@@ -6,7 +6,7 @@
 		public string Ramka => _model.Ramka;
         public string King => _model.King;
         public string Nick { get; set; }
-        public Visibility BV {  get; set; }
+        public Visibility BV { get; set; } = Visibility.Visible;
         public string Search
         {
             get { return GetValue<string>(); }
@@ -14,13 +14,23 @@
         }
         public int? Rating { get; set; }
         public int place { get; set; } = 1;
+        public int place2 { get; set; } = 1;
+
 
         public static ObservableCollection<LeaderboardModel> Players { get; set; } = new ObservableCollection<LeaderboardModel>();
+        public static ObservableCollection<LeaderboardModel> Players2 { get; set; } = new ObservableCollection<LeaderboardModel>();
 
 
         public LeaderboardViewModel()
         {
             _model.PropertyChanged += (s, e) => RaisePropertiesChanged(e.PropertyName);
+            var players = _model.GetAllUser().OrderByDescending(player => player.Rating);
+
+            foreach (var i in players)
+            {
+                Players2.Add(new LeaderboardModel(place2++, i.Nick, i.Rating));
+
+            }
             SearchPlayer();
         }
         public void SearchPlayer()
@@ -35,18 +45,22 @@
                 Players.Clear();
                 nick = nick.Where(p => p.Nick.ToString().ToLower().Contains(Search.ToLower())).ToList();
 
-                foreach (var i in nick)
+                foreach (var i in Players2)
                 {
-                    if (i != personWithMaxAge)
+                    if (!i.Nick.Contains(personWithMaxAge.Nick))
                     {
                         BV = Visibility.Collapsed;
-                        Players.Add(new LeaderboardModel(place, i.Nick, i.Rating));
+                        if (nick.Any(p => p.Nick == i.Nick))
+                            Players.Add(new LeaderboardModel(i.Place, i.Nick, i.Rating));
 
                     }
                     else
                     {
-                        BV = Visibility.Visible;
+                       
                         Players.Clear();
+                        BV = Visibility.Visible;
+                        //Players.Add(new LeaderboardModel(i.Place, i.Nick, i.Rating));
+
                     }
                 }
             }
