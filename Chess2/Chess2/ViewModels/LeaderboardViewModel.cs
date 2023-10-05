9478ac9e1340553d10d62b59857d0d18c7c62f42
Walkerbe-ai <akim.mustafaev.2016@mail.ru>
2023-10-05 18:48:04 +0500

    Добавление дизайна.

--- Chess2/Chess2/ViewModels/LeaderboardViewModel.cs
+++ Chess2/Chess2/ViewModels/LeaderboardViewModel.cs
@@ -10,7 +10,6 @@ namespace Chess2.ViewModels
         public string Nick { get; set; }
         public Visibility BV { get; set; } = Visibility.Visible;
         public Visibility SVV { get; set; } = Visibility.Visible;
-
         public string Search
         {
             get { return GetValue<string>(); }
@@ -18,11 +17,7 @@ namespace Chess2.ViewModels
         }
         public int? Rating { get; set; }
         public int place { get; set; } = 2;
-
-
         public static ObservableCollection<LeaderboardModel> Players { get; set; } = new ObservableCollection<LeaderboardModel>();
-
-
         public LeaderboardViewModel()
         {
             _model.PropertyChanged += (s, e) => RaisePropertiesChanged(e.PropertyName);
@@ -55,8 +50,6 @@ namespace Chess2.ViewModels
 				if (nick.Count == 0 && allCollapsed)
 					SVV = Visibility.Collapsed;
 				else SVV = Visibility.Visible;
-
-
 			}
 			else
             {
@@ -85,7 +78,6 @@ namespace Chess2.ViewModels
 			{
 				return x.Nick == y.Nick;
 			}
-
 			public int GetHashCode(LeaderboardModel obj)
 			{
 				return obj.Nick.GetHashCode();
