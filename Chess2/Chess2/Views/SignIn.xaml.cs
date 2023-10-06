f289892fe6a8d9ab74dd254711218da1906bf442
NuDa <92364838+NuDaVot@users.noreply.github.com>
2023-10-06 14:37:30 +0500

    Revert "delete"

---
+++ Chess2/Chess2/Views/SignIn.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Chess2.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для SignIn.xaml
+    /// </summary>
+    public partial class SignIn : Page
+    {
+        public SignIn()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
