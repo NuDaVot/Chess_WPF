60dec8d1f0a924a28052e9526e4af631a5defe3e
Walkerbe-ai <akim.mustafaev.2016@mail.ru>
2023-11-01 12:21:37 +0500

    Пофиксил ошибку с забанеными юзерами

--- Chess2/Chess2/ViewModels/LeaderboardViewModel.cs
+++ Chess2/Chess2/ViewModels/LeaderboardViewModel.cs
@@ -28,71 +28,70 @@ namespace Chess2.ViewModels
         }
         public void SearchPlayer()
         {
-			var personWithMaxAge = _model.GetAllUser().OrderByDescending(player => player.Rating).First();
-			Rating = personWithMaxAge.Rating;
-			Nick = personWithMaxAge.Nick;
-			var players = _model.GetAllUser().OrderByDescending(player => player.Rating);
-			var nick = _model.GetAllUser();
-			if (!string.IsNullOrWhiteSpace(Search))
+			try
 			{
-				nick = nick.Where(p => p.Nick.Contains(Search)).ToList();
-				var itemsToCollapse = Players.Except(
-							nick.Select(nick => new LeaderboardModel(0, "", 0, Visibility.Visible, 0) { Nick = nick.Nick }),
-							new MyItemComparer()
-						);
-				foreach (var pair in itemsToCollapse)
-				{
-					pair.Visibility = Visibility.Collapsed;
-				}
-				foreach (var item in Players)
-				{
-					if (nick.Any(item2 => item2.Nick == item.Nick))
-					{
-						// Если есть совпадение, устанавливаем Visibility в Visible
-						item.Visibility = Visibility.Visible;
-					}
-				}
-				if (personWithMaxAge.Nick.Contains(Search)) BV = Visibility.Visible;
-				else BV = Visibility.Collapsed;
-
-
-				bool allCollapsed = Players.All(item => item.Visibility == Visibility.Collapsed);
-
-				if (nick.Count == 0 && allCollapsed)
-				{
-					SVV = Visibility.Collapsed;
-					TBV = Visibility.Visible;
-				}
-				else
-				{
-					SVV = Visibility.Visible;
-					TBV = Visibility.Collapsed;
-				}
-			}
-			else
-            {
-                if(Players.Count == 0 ) 
+                var personWithMaxAge = _model.GetAllUser().OrderByDescending(player => player.Rating).First();
+                Rating = personWithMaxAge.Rating;
+                Nick = personWithMaxAge.Nick;
+                var players = _model.GetAllUser().OrderByDescending(player => player.Rating);
+                var nick = _model.GetAllUser();
+                if (!string.IsNullOrWhiteSpace(Search))
                 {
-                    foreach (var i in players)
+                    nick = nick.Where(p => p.Nick.Contains(Search)).ToList();
+                    var itemsToCollapse = Players.Except(
+                                nick.Select(nick => new LeaderboardModel(0, "", 0, Visibility.Visible, 0) { Nick = nick.Nick }),
+                                new MyItemComparer()
+                            );
+                    foreach (var pair in itemsToCollapse)
                     {
-                        if (i != personWithMaxAge)
-                            Players.Add(new LeaderboardModel(place++, i.Nick, i.Rating, Visibility.Visible, i.Iduser));
+                        pair.Visibility = Visibility.Collapsed;
+                    }
+                    foreach (var item in Players)
+                    {
+                        if (nick.Any(item2 => item2.Nick == item.Nick))
+                        {
+                            // Если есть совпадение, устанавливаем Visibility в Visible
+                            item.Visibility = Visibility.Visible;
+                        }
+                    }
+                    if (personWithMaxAge.Nick.Contains(Search)) BV = Visibility.Visible;
+                    else BV = Visibility.Collapsed;
+
+
+                    bool allCollapsed = Players.All(item => item.Visibility == Visibility.Collapsed);
+
+                    if (nick.Count == 0 && allCollapsed)
+                    {
+                        SVV = Visibility.Collapsed;
+                        TBV = Visibility.Visible;
+                    }
+                    else
+                    {
+                        SVV = Visibility.Visible;
+                        TBV = Visibility.Collapsed;
                     }
                 }
-		if else(Player == null)
-  		{
-    			TBV = Visibility.Visible;
-		}
                 else
                 {
-                    BV = Visibility.Visible;
-					SVV = Visibility.Visible;
-					TBV = Visibility.Collapsed;
-					Players.ForEach(item => item.Visibility = Visibility.Visible);
-
-				}
-
-			}          
+                    if (Players.Count == 0)
+                    {
+                        foreach (var i in players)
+                        {
+                            if (i != personWithMaxAge)
+                                Players.Add(new LeaderboardModel(place++, i.Nick, i.Rating, Visibility.Visible, i.Iduser));
+                        }
+                    }
+                    else
+                    {
+                        BV = Visibility.Visible;
+                        SVV = Visibility.Visible;
+                        TBV = Visibility.Collapsed;
+                        Players.ForEach(item => item.Visibility = Visibility.Visible);
+                    }
+                }
+            }
+            catch (InvalidOperationException ex) { BV = Visibility.Collapsed;SVV = Visibility.Collapsed; }
+			 
         }
         public class MyItemComparer : IEqualityComparer<LeaderboardModel>
 		{
