f8f4965c599ea68fb4a9cd98ef5fb4dd8c08b3b8
eRor <eror9911@gmail.com>
2023-11-17 21:58:39 +0500

    Админ Редактирование

--- Chess2/Chess2/Model/ProfileSetModel.cs
+++ Chess2/Chess2/Model/ProfileSetModel.cs
@@ -1,14 +1,48 @@
-﻿
-
-namespace Chess2.Model
+﻿namespace Chess2.Model
 {
     class ProfileSetModel : BindableBase
-	{
-		MainWindowModel _mainWindow;
+    {
+        MainWindowModel _mainWindow;
+        HistoryChessContext _historyChessContext;
 
-		public ProfileSetModel()
-		{
-			_mainWindow = MainWindowViewModel._metod;
-		}
-	}
+        public ProfileSetModel()
+        {
+            _mainWindow = MainWindowViewModel._metod;
+            _historyChessContext = new HistoryChessContext();
+        }
+        public void IsCansel()
+        {
+            _mainWindow.Navipage("MenuA.xaml");
+        }
+        public async Task redactUser(User UserCst, string oldPassword)
+        {
+            if (VerifyPassword(oldPassword, UserSetting.Default.Password))
+            {
+                User? user = await _historyChessContext.Users.FirstOrDefaultAsync(user => user.Iduser == UserSetting.Default.IdUser);
+                if (user != null)
+                {
+                    user.Login = UserCst.Login;
+                    user.Nick = UserCst.Nick;
+                    user.Password = UserCst.Password;
+                    await _historyChessContext.SaveChangesAsync();
+                    MessageBox.Show("The changes are saved, please re-log in");
+                    _mainWindow.Navipage("MenuA.xaml");
+                }
+                else
+                {
+                    MessageBox.Show("Something went wrong, the changes are not saved, please re-log in with your old password or register a new account");
+                    _mainWindow.Navipage("MenuA.xaml");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Incorrect password");
+            }
+        }
+        public static bool VerifyPassword(string enteredPassword, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(enteredPassword, hashedPassword);
+        }
+        public List<User> GetAllUserNick() => _historyChessContext.Users.ToList();
+    }
 }
--- Chess2/Chess2/ViewModels/ProfileSetViewModel.cs
+++ Chess2/Chess2/ViewModels/ProfileSetViewModel.cs
@@ -1,14 +1,18 @@
 ﻿using Microsoft.VisualBasic.Logging;
+using Chess2.Data;
+using System.Security.Cryptography;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace Chess2.ViewModels
 {
-   public class ProfileSetViewModel : BindableBase
-   {
-		readonly ProfileSetModel _model = new ProfileSetModel();
+    public class ProfileSetViewModel : BindableBase
+    {
+        readonly ProfileSetModel _model = new ProfileSetModel();
 
-        public User UserCst { get; set; }
+        public User UserCst = new User();
 
-        public ObservableCollection<User> UsersCollect { get; set; }
+        public List<User> UsersCollect { get; set; }
 
         public string Nick { get; set; }
 
@@ -16,38 +20,52 @@ namespace Chess2.ViewModels
 
         public string Password { get; set; }
 
-        public string ConfirmPass { get; set; }
+        public string NewPassword { get; set; }
+
+        public string ConfirmNewPass { get; set; }
 
         public int Iduser { get; set; }
 
         public ProfileSetViewModel()
         {
-			_model.PropertyChanged += (s, e) => RaisePropertiesChanged(e.PropertyName);
-            //UsersCollect = _model.getAllUsers();
-            Login = UserSetting.Default.Login;
-            Nick = UserSetting.Default.Nick;
-            Password = UserSetting.Default.Password;
+            _model.PropertyChanged += (s, e) => RaisePropertiesChanged(e.PropertyName);
+            var users = _model.GetAllUserNick();
+            UsersCollect = users;
         }
-        //public AsyncCommand EditCommand => new(async () =>
-        //{
-        //    UserCst.Login = Login;
-        //    UserCst.Password = Password;
-        //    UserCst.Nick = Nick;
-        //    UserCst.Iduser = Iduser;
-
-        //    await _model.redactUser(UserCst, UsersCollect);
-        //}, bool () =>
-        //{
-        //    if (string.IsNullOrWhiteSpace(Login)
-        //    || string.IsNullOrWhiteSpace(Password)
-        //    || string.IsNullOrWhiteSpace(Nick))
-        //        return false;
-        //    else if(ConfirmPass != Password)
-        //    {
-        //        return false;
-        //    }
-        //    else return true;
-        //});
-        //public DelegateCommand MenuCommand => new(() => _model.isGoBack());
+        public DelegateCommand CanselCommand => new(() => _model.IsCansel());
+        public AsyncCommand EditCommand => new(async () =>
+        {
+            string salt = BCrypt.Net.BCrypt.GenerateSalt(12);
+            string hashedNewPassword = BCrypt.Net.BCrypt.HashPassword(NewPassword, salt);
+            UserCst.Login = Login;
+            UserCst.Password = hashedNewPassword;
+            UserCst.Nick = Nick;
+            UserCst.Iduser = Iduser;
+            await _model.redactUser(UserCst, Password);
+        }, bool () =>
+        {
+            if (string.IsNullOrWhiteSpace(Login)
+            || string.IsNullOrWhiteSpace(Password)
+            || string.IsNullOrWhiteSpace(Nick)
+            || string.IsNullOrWhiteSpace(NewPassword)
+            || string.IsNullOrWhiteSpace(ConfirmNewPass))
+                return false;
+            else if (UsersCollect.Any(i =>
+            i.Login == Login && i.Iduser != UserSetting.Default.IdUser))
+            {
+                return false;
+            }
+            else if (UsersCollect.Any(i =>
+            i.Nick == Nick && i.Iduser != UserSetting.Default.IdUser))
+            {
+                return false;
+            }
+            else if (ConfirmNewPass != NewPassword)
+            {
+                return false;
+            }
+            else
+                return true;
+        });
     }
 }
